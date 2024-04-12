f7d557ee8fa4fa2a6493ae9717f90a7fc607f8f9
Nicolas Bottarini <nicolasbottarini@gmail.com>
2024-04-12 09:58:41 -0300

    use maven central to deploy plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,57 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     `kotlin-dsl`
-    id("com.gradle.plugin-publish") version "1.2.1"
+    id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
-group = "dev.botta.kotlin-conventions"
-version = "1.0.0"
+group = "dev.botta"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
-    gradlePluginPortal()
 }
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
 }
 
-gradlePlugin {
-    website = "https://github.com/nbottarini/kotlin-conventions-gradle-plugin"
-    vcsUrl = "https://github.com/nbottarini/kotlin-conventions-gradle-plugin.git"
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+    signAllPublications()
+    coordinates(project.group.toString(), "kotlin-conventions", project.version.toString())
 
-    plugins {
-        matching { it.name == "dev.botta.kotlin-conventions" }.configureEach {
-            displayName = "dev.botta.kotlin-conventions"
-            description = "A plugin that applies Kotlin conventions"
-            tags = listOf("kotlin", "conventions")
+    pom {
+        name.set("Kotlin Conventions Gradle Plugin")
+        description.set("A plugin that applies Kotlin conventions")
+        inceptionYear.set("2024")
+        url.set("https://github.com/nbottarini/kotlin-conventions-gradle-plugin")
+
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("http://www.opensource.org/licenses/mit-license.php")
+                distribution.set("http://www.opensource.org/licenses/mit-license.php")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("nbottarini")
+                name.set("Nicolas Bottarini")
+                url.set("https://github.com/nbottarini/")
+                email.set("nicolasbottarini@gmail.com")
+            }
+        }
+
+        scm {
+            connection.set("scm:git:git://github.com/nbottarini/kotlin-conventions-gradle-plugin.git")
+            developerConnection.set("scm:git:ssh://github.com/nbottarini/kotlin-conventions-gradle-plugin.git")
+            url.set("https://github.com/nbottarini/kotlin-conventions-gradle-plugin")
         }
     }
+}
+
+tasks.named<Copy>("processResources") {
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
 }
